2525e682a583a481dd8700ca73f5346bedbc97df
Simon Raik-Allen <shimmie@shimmie.com>
2021-09-03 11:57:43 +1000

    Fixed some minor typing issues.

--- src/eventbase.ts
+++ src/eventbase.ts
@@ -1,59 +1,59 @@
 //
 // TODO: encapsulate the underlying database elsewhere
 //
-import pg from 'pg'
-const { Client } = pg
-import Event from './event'
+import pg from 'pg';
+const { Client } = pg;
+import Event from './event';
 
 export interface IEventBase {
-    getAllEventsInOrder: () => Promise<any>
-    runQuery: (query: string, values: string[] | undefined) => Promise<any>
-    connect: () => Promise<void>
-    close: () => Promise<void>
-    addEvent: (event: Event) => Promise<any[]>
-    showTables: () => Promise<any[]>
-    createTables: () => Promise<any[]>
-    dropTables: () => Promise<any[]>
+    getAllEventsInOrder: () => Promise<any>;
+    runQuery: (query: string, values: string[] | undefined) => Promise<any>;
+    connect: () => Promise<void>;
+    close: () => Promise<void>;
+    addEvent: (event: Event) => Promise<any[]>;
+    showTables: () => Promise<any[]>;
+    createTables: () => Promise<any[]>;
+    dropTables: () => Promise<any[]>;
 }
 
 export default function Eventbase(ConnectionString: string): IEventBase {
     if (!ConnectionString) {
-        throw new Error('Missing DATABASE_URL environment variable.')
+        throw new Error('Missing DATABASE_URL environment variable.');
     } else {
-        console.info('Eventbase connection string: ', ConnectionString)
+        console.info('Eventbase connection string: ', ConnectionString);
     }
 
     const connection = new Client({
         connectionString: ConnectionString,
-    })
+    });
 
     // called during start up to first connect to the database
     // TODO: retry to solve docker start up timing issue
     const connect = async () => {
-        await connection.connect()
-    }
+        await connection.connect();
+    };
 
     const close = async () => {
-        await connection.end()
-    }
+        await connection.end();
+    };
 
     const addEvent = (event: Event) => {
         const query =
-            'INSERT into eventlist(StreamId, Data, Type, Meta) VALUES($1, $2, $3, $4) RETURNING SequenceNum, streamId, logdate, type'
+            'INSERT into eventlist(StreamId, Data, Type, Meta) VALUES($1, $2, $3, $4) RETURNING SequenceNum, streamId, logdate, type';
         const values: string[] = [
             event.streamId,
             JSON.stringify(event.data),
             event.type,
             JSON.stringify(event.meta),
-        ]
-        return runQuery(query, values)
-    }
+        ];
+        return runQuery(query, values);
+    };
 
     // Get all events in the correct squence for replay
     const getAllEventsInOrder = () => {
-        const query = 'SELECT * FROM eventlist ORDER BY SequenceNum'
-        return runQuery(query)
-    }
+        const query = 'SELECT * FROM eventlist ORDER BY SequenceNum';
+        return runQuery(query);
+    };
 
     const runQuery = async (
         query: string,
@@ -61,15 +61,15 @@ export default function Eventbase(ConnectionString: string): IEventBase {
     ) => {
         try {
             if (values) {
-                return (await connection.query(query, values)).rows
+                return (await connection.query(query, values)).rows;
             } else {
-                return (await connection.query(query)).rows
+                return (await connection.query(query)).rows;
             }
         } catch (err: any) {
-            console.error(`Query error <${query}> [${values}]: ${err.message}`)
-            throw err
+            console.error(`Query error <${query}> [${values}]: ${err.message}`);
+            throw err;
         }
-    }
+    };
 
     //
     //  Create the tables required for the event store.
@@ -85,27 +85,27 @@ export default function Eventbase(ConnectionString: string): IEventBase {
             Meta jsonb NOT NULL,
             LogDate timestamptz NOT NULL DEFAULT now(),
             PRIMARY KEY (SequenceNum)
-        );`
+        );`;
 
-        return runQuery(queryString)
-    }
+        return runQuery(queryString);
+    };
 
     const dropTables = () => {
-        const query = 'DROP TABLE IF EXISTS eventlist'
+        const query = 'DROP TABLE IF EXISTS eventlist';
         try {
-            return runQuery(query)
-        } catch (err) {
-            throw new Error(`Error dropping database tables: ${err.message}`)
+            return runQuery(query);
+        } catch (err: any) {
+            throw new Error(`Error dropping database tables: ${err.message}`);
         }
-    }
+    };
 
     // filter out system tables
     const showTables = () => {
         const query = `
             SELECT * FROM pg_catalog.pg_tables 
-            WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';`
-        return runQuery(query)
-    }
+            WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';`;
+        return runQuery(query);
+    };
 
     return {
         getAllEventsInOrder,
@@ -116,5 +116,5 @@ export default function Eventbase(ConnectionString: string): IEventBase {
         showTables,
         createTables,
         dropTables,
-    }
+    };
 }
