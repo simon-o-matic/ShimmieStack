ab1b2ab67d02a5a16ef32797ec7f2ebf9cf9de2c
Simon Raik-Allen <shimmie@shimmie.com>
2021-10-23 14:12:00 +1100

    Added a test for overridig the admin mount point.

--- src/tests/routes.test.ts
+++ src/tests/routes.test.ts
@@ -8,6 +8,7 @@ describe('when mounting a processor', () => {
         const routerMock = jest.fn() as unknown as Router;
         mountApi(appMock, 'blah', '/blah', routerMock);
     });
+
     it('and its a duplicate it should throw', async () => {
         const appMock = jest.fn() as unknown as Application;
         appMock.use = jest.fn();
@@ -15,6 +16,15 @@ describe('when mounting a processor', () => {
         mountApi(appMock, 'blah', '/boo', routerMock);
         expect(() => mountApi(appMock, 'blah', '/boo', routerMock)).toThrow(
             'Mount point duplicate: /boo'
+        );
+    });
+
+    it('and its call /admin it should thro', async () => {
+        const appMock = jest.fn() as unknown as Application;
+        appMock.use = jest.fn();
+        const routerMock = jest.fn() as unknown as Router;
+        expect(() => mountApi(appMock, 'blah', '/admin', routerMock)).toThrow(
+            '"/admin" mount point is reserved'
         );
     });
 });
