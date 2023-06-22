941c6a51a83bd773d5da77cbffe84f6f829faac8
James Boyce <james@muso.live>
2023-06-22 17:04:11 +1000

    Add a test to check unique versionIds

---
+++ src/tests/object-versioning.test.ts
@@ -0,0 +1,49 @@
+import ShimmieTestStack from '../shimmieteststack'
+import { Meta, TypedEvent } from '../event'
+import { expect, describe, it } from '@jest/globals'
+
+type CommandEventModels = {
+    EXAMPLE_EVENT: {
+        data: string
+    },
+}
+
+type QueryEventModels = CommandEventModels
+
+const testStack = ShimmieTestStack<CommandEventModels, QueryEventModels>()
+
+const meta: Meta = {
+    user: 'testguy',
+    userAgent: 'blah',
+    date: Date.now()
+}
+
+describe('Object Versioning', () => {
+    it('should generate a unique object version on each recordevent call', async () => {
+        let recordedEvents: TypedEvent<'EXAMPLE_EVENT',CommandEventModels['EXAMPLE_EVENT']>[] = []
+        testStack.subscribe('EXAMPLE_EVENT', (event) => {
+            recordedEvents.push(event)
+        })
+
+        await testStack.recordEvent(
+            'exampleStreamId',
+            'EXAMPLE_EVENT',
+            { data: 'something' },
+            meta
+        )
+
+        await testStack.recordEvent(
+            'exampleStreamId',
+            'EXAMPLE_EVENT',
+            { data: 'something else' },
+            meta
+        )
+
+        expect(recordedEvents.length).toEqual(2)
+
+        // does each event have its own unique versionid?
+        const uniqueversionIds = new Set(recordedEvents.map(event => event.streamVersionId))
+        expect([...uniqueversionIds].length).toEqual(recordedEvents.length)
+
+    })
+})
